1fdb7611c1614d1baab81c4f2199764cbc1a9235
Phoneguytech75 <shahidhusaindada@outlook.com>
2021-10-23 09:35:00 +0100

    Intial Commit

---
+++ ph.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string.h>
+#include <direct.h>
+#include <fstream> 
+#include <filesystem>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  if(strcmp(argv[1],"new")==0){
+    cout << "Creating new project. Note that the file will be created in the folder you are in.";
+    std::ofstream outfile ("main.ph");
+    outfile << "send('Hello World!')" << std::endl;
+    outfile.close();
+    system("code .");
+} else if (strcmp(argv[1],"shell")==0){
+      system("python \"C:/Phoneguytech/PH_Code/something.py\"");
+  }
+}
+
